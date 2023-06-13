9b5160ec5a100f7bbb6c2d9aa03ca5f7b81f1dc8
marinahalim7 <marinahalim7@gmail.com>
2023-06-13 20:39:18 +0300

    User_drugs Crud Operation

--- ServerSide/app/Models/UserDrug.php
+++ ServerSide/app/Models/UserDrug.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserDrug extends Model
 {
-    protected $table = 'user_drugs';
     protected $fillable=['name','exp_date','price','quantity','publishable'];
     
     public function users(){
