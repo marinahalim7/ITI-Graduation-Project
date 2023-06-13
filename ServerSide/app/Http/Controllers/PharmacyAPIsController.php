26871140755dd5dd696d88eb7d9ba033fcc80a86
Mohammed Aldghar <mohammedaldghar5@gmail.com>
2023-06-13 15:29:19 +0300

    Store Controller

---
+++ ServerSide/app/Http/Controllers/PharmacyAPIsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pharmacy;
+use Illuminate\Http\Request;
+
+class PharmacyAPIsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pharmacy $pharmacy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pharmacy $pharmacy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pharmacy $pharmacy)
+    {
+        //
+    }
+}
