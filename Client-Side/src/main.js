a60661e245acdb22b23c79e84f6d8c21c95f77e6
Mohammed Aldghar <mohammedaldghar5@gmail.com>
2023-06-21 19:26:44 +0300

    add bootstrap as a package

--- Client-Side/src/main.js
+++ Client-Side/src/main.js
@@ -1,6 +1,9 @@
 import './assets/main.css'
 import router from "@/routers";
 import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '@popperjs/core';
 import App from './App.vue'
 import {createPinia} from "pinia";
 const pinia = createPinia();
